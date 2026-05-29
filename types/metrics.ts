35acf8873333fb8acafd5e29502915073cb92fd0
fred <fred@freds-MacBook-Air.local>
2026-05-30 00:19:11 +0200

    feat: add GymSession TypeScript types

---
+++ types/metrics.ts
@@ -0,0 +1,32 @@
+export type ConsoleType = 'rowing' | 'cycling' | 'unknown'
+
+export interface BaseSession {
+  consoleType: ConsoleType
+  capturedAt: string
+  imageDataUrl?: string
+}
+
+export interface RowingSession extends BaseSession {
+  consoleType: 'rowing'
+  duration: string
+  distanceMeters: number
+  splitPer500m: string
+  strokeRate: number
+  calories: number
+  watts: number
+  heartRate?: number
+}
+
+export interface CyclingSession extends BaseSession {
+  consoleType: 'cycling'
+  duration: string
+  distanceKm: number
+  avgWatts: number
+  avgRpm: number
+  avgSpeedKmh: number
+  calories: number
+  heartRate?: number
+  resistanceLevel?: number
+}
+
+export type GymSession = RowingSession | CyclingSession | (BaseSession & { consoleType: 'unknown' })
